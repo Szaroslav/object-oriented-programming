6f0bbabdf770f2269d234007f8c2e673f06ab0bd
Jakub <jakub.szaredko@gmail.com>
2022-12-12 11:35:59 +0100

    Init project 1

---
+++ src/main/java/agh/ics/project1/Organism.java
@@ -0,0 +1,11 @@
+package agh.ics.project1;
+
+import agh.ics.oop.Vector2d;
+
+public class Organism {
+    private Vector2d position;
+
+    public Vector2d getPosition() {
+        return position;
+    }
+}
