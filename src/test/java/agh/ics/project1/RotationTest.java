a5b69838e47ff1e26d30490382c95b1fe4f6285f
Jakub <jakub.szaredko@gmail.com>
2022-12-16 19:05:58 +0100

    Create rotation unit tests

---
+++ src/test/java/agh/ics/project1/RotationTest.java
@@ -0,0 +1,22 @@
+package agh.ics.project1;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class RotationTest {
+    @Test
+    public void testRotate() {
+        out.println("Testing Rotation.rotate(int)");
+        Rotation rotation = Rotation.NORTH;
+
+        assertEquals(Rotation.NORTH_EAST, rotation.rotate(1));
+        assertEquals(Rotation.SOUTH_WEST, rotation.rotate(5));
+        assertEquals(Rotation.NORTH, rotation.rotate(8));
+        assertEquals(Rotation.NORTH_WEST, rotation.rotate(-1));
+        assertEquals(Rotation.SOUTH_WEST, rotation.rotate(-3));
+        assertEquals(Rotation.NORTH_WEST, rotation.rotate(-9));
+    }
+}
