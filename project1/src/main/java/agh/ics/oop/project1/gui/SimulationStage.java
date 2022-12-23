37bc481093e34424b4540df8c7d445dbedddddfe
Jakub <jakub.szaredko@gmail.com>
2022-12-23 18:45:29 +0100

    Update SimulationStage, add lion icon as animal representation

--- project1/src/main/java/agh/ics/oop/project1/gui/SimulationStage.java
+++ project1/src/main/java/agh/ics/oop/project1/gui/SimulationStage.java
@@ -1,12 +1,12 @@
 package agh.ics.oop.project1.gui;
 
+import agh.ics.oop.project1.AbstractOrganism;
+import agh.ics.oop.project1.animal.Animal;
 import agh.ics.oop.project1.plant.Plant;
-import agh.ics.oop.project1.utils.Vector2d;
 import agh.ics.oop.project1.world.engine.IEngineObserver;
 import agh.ics.oop.project1.world.maps.AbstractMap;
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
@@ -16,18 +16,21 @@ import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 public class SimulationStage extends Stage implements IEngineObserver {
     private final int WIDTH = 800;
     private final int HEIGHT = 600;
     private final int CELL_SIZE;
-    private GridPane board;
-    private AbstractMap map;
-    private Image plantTexture;
+    private final GridPane board;
+    private final AbstractMap map;
+    private final Image plantTexture;
+    private final Image animalIcon;
 
     public SimulationStage(AbstractMap map) throws FileNotFoundException {
         board = new GridPane();
         plantTexture = new Image(new FileInputStream("src/main/resources/textures/grass.png"));
+        animalIcon = new Image(new FileInputStream("src/main/resources/icons/lion.png"));
         this.map = map;
 
         CELL_SIZE = Math.min(WIDTH / map.getWidth(), HEIGHT / map.getHeight());
@@ -52,12 +55,17 @@ public class SimulationStage extends Stage implements IEngineObserver {
     private void renderBoard() {
         Platform.runLater(() -> {
             board.getChildren().clear();
-            for (Plant plant : map.getPlantsList()) {
-                ImageView imageView = new ImageView(plantTexture);
-                imageView.setFitWidth(CELL_SIZE);
-                imageView.setFitHeight(CELL_SIZE);
-                board.add(imageView, plant.getPosition().x, fromGameToBoardY(plant.getPosition().y));
-            }
+            renderOrganisms(map.getAnimalsList(), animalIcon);
+            renderOrganisms(map.getPlantsList(), plantTexture);
         });
+    }
+
+    private void renderOrganisms(List<? extends AbstractOrganism> organismsList, Image image) {
+        for (AbstractOrganism organism : organismsList) {
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(CELL_SIZE);
+            imageView.setFitHeight(CELL_SIZE);
+            board.add(imageView, organism.getPosition().x, fromGameToBoardY(organism.getPosition().y));
+        }
     }
 }
